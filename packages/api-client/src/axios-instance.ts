8ff6112483c6effd5190ccbb98446d423808fbec
Phathdt <phathdt379@gmail.com>
2026-05-17 23:07:01 +0700

    fix(api-client): override oxlint to prefer type-imports for verbatimModuleSyntax compatibility

--- packages/api-client/src/axios-instance.ts
+++ packages/api-client/src/axios-instance.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios';
 
 const DEFAULT_BASE_URL = '/api';
 const DEFAULT_TIMEOUT_MS = 30_000;
